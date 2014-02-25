ab2be1709a934899e0261ddc53e9a7b91edb5ece
Mark J Smith <mark@markjs.net>
2014-02-25 19:23:37 +0000

    Do some cool URL stuff in index.php

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
-<? echo 'hello, world'; ?>
-<br>
-<? echo $_SERVER['REQUEST_URI']; ?>
+<?php
+
+$BASE_PATH = '/atwd/';
+$REQUEST_PATH = substr($_SERVER['REQUEST_URI'], strlen($BASE_PATH));
+
+
+
+echo $REQUEST_PATH;
