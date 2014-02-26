1246c8c919c6367b4df1c0fa1b24259294956b18
Mark J Smith <mark@markjs.net>
2014-02-26 18:39:17 +0000

    Add a couple of handy TODO reminders

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 $BASE_PATH = '/atwd/';
 $REQUEST_PATH = substr($_SERVER['REQUEST_URI'], strlen($BASE_PATH));
 
+#TODO: Real regex here
 if (preg_match('/^crimes\//', $REQUEST_PATH)) {
   require 'app/get/all_crimes.php';
 }
