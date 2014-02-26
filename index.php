d2f6cb3175dcf61975cf9fb027706588cfa8701f
Mark J Smith <mark@markjs.net>
2014-02-26 21:56:17 +0000

    Add regex route for region crimes get

--- index.php
+++ index.php
@@ -4,6 +4,10 @@ $BASE_PATH = '/atwd/';
 $REQUEST_PATH = substr($_SERVER['REQUEST_URI'], strlen($BASE_PATH));
 
 #TODO: Real regex here
-if (preg_match('/^crimes\//', $REQUEST_PATH)) {
+if (preg_match('/^crimes\/[0-9,-]+\/xml|json/', $REQUEST_PATH)) {
   require 'app/get/all_crimes.php';
+}
+
+if (preg_match('/^crimes\/[0-9,-]+\/[a-z,_]+\/xml|json/', $REQUEST_PATH)) {
+  require 'app/get/region_crimes.php';
 }
