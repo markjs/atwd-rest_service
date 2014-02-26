3919bb3d0f9d9a86772414843a1698d1baa61e36
Mark J Smith <mark@markjs.net>
2014-02-26 19:44:31 +0000

    Account correctly for england and wales for inconsistent json according to spec

--- app/views/all_crimes.json.php
+++ app/views/all_crimes.json.php
@@ -13,24 +13,19 @@ $crimes['year'] = $date_range;
 foreach ($values as $value) {
   $item = array();
 
-  if (isset($value['id']) && strlen($value['id']) > 0) {
-    $item['id'] = ''.$value['id'];
-  }
-
-  if (isset($value['total']) && strlen($value['total']) > 0) {
-    $item['total'] = ''.$value['total'];
-  }
-
-  if (isset($crimes[$value['element']])) {
-    $crimes[$value['element']][] = $item;
+  if (!preg_match('/region|national/', $value['element'])) {
+    $crimes[$value['element']] = array('total' => ''.$value['total']);
   } else {
-    $crimes[$value['element']] = array($item);
+    $item['id'] = ''.$value['id'];
+    $item['total'] = ''.$value['total'];
+
+    if (isset($crimes[$value['element']])) {
+      $crimes[$value['element']][] = $item;
+    } else {
+      $crimes[$value['element']] = array($item);
+    }
   }
 }
-
-
-
-
 
 $response['crimes'] = $crimes;
 echo json_encode(array('response' => $response));
