a7de4b628166ade3d4ba273540c51a89aab68a7e
Mark J Smith <mark@markjs.net>
2014-02-26 18:30:13 +0000

    Loop values into xml view

--- app/views/all_crimes.xml.php
+++ app/views/all_crimes.xml.php
@@ -5,7 +5,16 @@ $dom = new DOMDocument;
 $response = $dom->createElement('response');
 $response->setAttribute('timestamp', time());
 
+foreach ($values as $value) {
+  $element = $dom->createElement($value['element']);
+  if (strlen($value['id']) > 0) {
+    $element->setAttribute('id', $value['id']);
+  }
+  $element->setAttribute('total', $total);
 
+  $response->appendChild($element);
+}
 
 $dom->appendChild($response);
+$dom->formatOutput = true;
 echo $dom->saveXML();
