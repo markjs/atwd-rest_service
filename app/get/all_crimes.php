9f4267ea4e43ef01bb07a196cf9e58128a4aa95e
Mark J Smith <mark@markjs.net>
2014-02-26 17:57:35 +0000

    Start of all crimes get

---
+++ app/get/all_crimes.php
@@ -0,0 +1,19 @@
+<?php
+
+$xml = simplexml_load_file('data/recorded_crime.xml');
+
+$values = array();
+
+foreach ($xml->children() as $region) {
+  $region_name = $region->name;
+
+  $total = 0;
+
+  foreach ($region->children() as $area) {
+    $total += $area->total_recorded_crime->including_fraud;
+  }
+
+  $element_name = 'region';
+
+  $values[] = array('element' => $element_name, 'id' => $region_name, 'total' => $total);
+}
