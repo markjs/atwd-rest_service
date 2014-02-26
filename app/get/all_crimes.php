eeece4c4fc0939b282b8943062d3a15589b28808
Mark J Smith <mark@markjs.net>
2014-02-26 18:05:44 +0000

    Add some cases to handle different element names

--- app/get/all_crimes.php
+++ app/get/all_crimes.php
@@ -5,20 +5,33 @@ $xml = simplexml_load_file('data/recorded_crime.xml');
 $values = array();
 
 foreach ($xml->children() as $region) {
-  $region_name = $region->name;
-
   $total = 0;
 
   foreach ($region->children() as $area) {
     $total += $area->total_recorded_crime->including_fraud;
   }
 
-  $element_name = 'region';
+  $region_name = $region->name;
+
+
+  switch ($region_name) {
+    case 'Wales':
+      $element_name = 'wales';
+      $region_name = null;
+      break;
+    case 'British Transport Police':
+    case 'Action Fraud':
+      $element_name = 'national';
+      break;
+    default:
+      $element_name = 'region';
+  }
+
 
   $values[] = array('element' => $element_name, 'id' => $region_name, 'total' => $total);
 }
 
 foreach ($values as $value) { ?>
-  <<?php echo $value['element'] ?> id="<?php echo $value['id'] ?>" total="<?php echo $value['total'] ?>">
+  <<?php echo $value['element'] ?> id="<?php echo $value['id'] ?>" total="<?php echo $value['total'] ?>" />
 <?php
 }
