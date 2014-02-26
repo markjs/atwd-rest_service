f7f0f544ebc9d659e46003c9dcff3abdeb33c8bf
Mark J Smith <mark@markjs.net>
2014-02-26 19:36:57 +0000

    Most of get all json view

--- app/views/all_crimes.json.php
+++ app/views/all_crimes.json.php
@@ -1,1 +1,36 @@
-all crimes json
+<?php
+
+header('Content-Type: text/json');
+
+$response = array();
+
+# Cast time to string to match spec example
+$response['timestamp'] = ''.time();
+
+$crimes = array();
+$crimes['year'] = $date_range;
+
+foreach ($values as $value) {
+  $item = array();
+
+  if (isset($value['id']) && strlen($value['id']) > 0) {
+    $item['id'] = ''.$value['id'];
+  }
+
+  if (isset($value['total']) && strlen($value['total']) > 0) {
+    $item['total'] = ''.$value['total'];
+  }
+
+  if (isset($crimes[$value['element']])) {
+    $crimes[$value['element']][] = $item;
+  } else {
+    $crimes[$value['element']] = array($item);
+  }
+}
+
+
+
+
+
+$response['crimes'] = $crimes;
+echo json_encode(array('response' => $response));
