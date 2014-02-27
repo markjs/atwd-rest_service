3f8f5eb8345e5ef31978a7d3a7fe1e0ad8c6c776
Mark J Smith <mark@markjs.net>
2014-02-27 09:00:46 +0000

    Passive aggressive message about value persistence

--- app/put/region_total.php
+++ app/put/region_total.php
@@ -13,6 +13,10 @@ foreach ($region->children() as $area) {
   $previous_total += $area->total_recorded_crime->including_fraud;
 }
 
+# Note: Persisting the new region value would happen here, but the spec mentions
+# nothing about that and extra totals would need to be calculated so it's being
+# counted as unnecessary now.
+
 if (file_exists("app/views/region_total.$request_format.php")) {
   require "app/views/region_total.$request_format.php";
 } else {
