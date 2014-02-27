4158a36674f1ba669c802d3c48ffc20ada2ae5c7
Mark J Smith <mark@markjs.net>
2014-02-27 10:04:31 +0000

    Start of visualiser code

--- client/app.js
+++ client/app.js
@@ -1,1 +1,11 @@
-console.log('hello, world');
+$form = $('#visualiser-form');
+
+$form.on('submit', function(e) {
+  e.preventDefault();
+  timePeriod = $form.find('#time-period').val();
+  regionSlug = $form.find('#region').val();
+
+  url = "../crimes/" + timePeriod + "/" + regionSlug + "/json";
+
+  $.get(url, function(data) { console.log(data); });
+});
