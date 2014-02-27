eea48daf42971422ef880e047a84a48d6d061aed
Mark J Smith <mark@markjs.net>
2014-02-27 04:07:32 +0000

    Fix regex to correctly match response type

--- index.php
+++ index.php
@@ -3,13 +3,13 @@
 $BASE_PATH = '/atwd/';
 $REQUEST_PATH = substr($_SERVER['REQUEST_URI'], strlen($BASE_PATH));
 
-#TODO: Real regex here
-if (preg_match('/^crimes\/[0-9,-]+\/xml|json/', $REQUEST_PATH)) {
+
+if (preg_match('/^crimes\/[0-9,-]+\/(xml|json)$/', $REQUEST_PATH)){
   require 'app/get/all_crimes.php';
   return;
 }
 
-if (preg_match('/^crimes\/[0-9,-]+\/[a-z,_]+\/xml|json/', $REQUEST_PATH)) {
+if (preg_match('/^crimes\/[0-9,-]+\/[a-z,_]+\/(xml|json)$/', $REQUEST_PATH)) {
   require 'app/get/region_crimes.php';
   return;
 }
