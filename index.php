d220ffd7f2863a6dfc3ce786f869f54c51c1d6a1
Mark J Smith <mark@markjs.net>
2014-02-27 03:33:12 +0000

    Return so only one route is run

--- index.php
+++ index.php
@@ -6,8 +6,10 @@ $REQUEST_PATH = substr($_SERVER['REQUEST_URI'], strlen($BASE_PATH));
 #TODO: Real regex here
 if (preg_match('/^crimes\/[0-9,-]+\/xml|json/', $REQUEST_PATH)) {
   require 'app/get/all_crimes.php';
+  return;
 }
 
 if (preg_match('/^crimes\/[0-9,-]+\/[a-z,_]+\/xml|json/', $REQUEST_PATH)) {
   require 'app/get/region_crimes.php';
+  return;
 }
